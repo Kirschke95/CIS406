289bf55ea34b2a7c49f8c4e39177375d8f3ff36b
ucctn <chetan.singhal@ucertify.com>
2023-04-04 07:01:15 -0700

    This is a test commit

---
+++ courseSetup/src/courseSetup/CourseSetup.java
@@ -0,0 +1,12 @@
+package courseSetup;
+
+public class CourseSetup {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println();
+		System.out.println("Congratulations on your first Java program");
+
+	}
+
+}
